31277ba0c1ef3938126252e58d11881bbda4ce54
Your Name <joyywamaitha@gmail.com>
2022-06-16 19:51:08 +0300

    fragments

--- app/src/main/java/com/wamaithajoy/workoutlog/HomeActivity.kt
+++ app/src/main/java/com/wamaithajoy/workoutlog/HomeActivity.kt
@@ -2,10 +2,41 @@ package com.wamaithajoy.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.SurfaceControl
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var bottom_navigation: BottomNavigationView
+    lateinit var fcvHome: FragmentContainerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        castViews()
+        setupBottomNav()
+    }
+    fun castViews(){
+    bottom_navigation=findViewById(R.id.bottom_navigation)
+    fcvHome=findViewById(R.id.fcvHome)
+    }
+    fun setupBottomNav(){
+    bottom_navigation.setOnItemSelectedListener { item->
+        when(item.itemId){
+            R.id.plan ->{
+                supportFragmentManager.beginTransaction().replace(R.id.fcvHome,PlanFragment()).commit()
+                true
+            }
+            R.id.track ->{
+                supportFragmentManager.beginTransaction().replace(R.id.fcvHome,TrackFragment()).commit()
+                true
+            }
+            R.id. profile ->{
+                val transaction=supportFragmentManager.beginTransaction().replace(R.id.fcvHome,ProfileFragment()).commit()
+                true
+            }
+            else->false
+        }
+    }
     }
 }
